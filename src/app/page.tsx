afc5b1cb9edbe5a7984cedef0637f12bf1adf7ca
Jony-2002 <you@example.com>
2023-07-18 06:48:46 -0700

    added About section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-import Filter from "@/components/Filter/Filter"
 import MainContent from "@/components/MainContent/MainContent"
+import About from "@/components/About/About"
+import Filter from "@/components/Filter/Filter"
 import MainSlider from "@/components/MainSlider/MainSlider"
 
 export default function Home() {
   return (
     <main>
       <MainSlider />
+      <About />
       <Filter />
       <MainContent />
     </main>
