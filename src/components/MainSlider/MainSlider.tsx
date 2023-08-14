9eee704f67f5395346f1525a58a3c505ae46dba9
Jony-2002 <you@example.com>
2023-08-14 10:11:37 -0700

    class conflicts

--- src/components/MainSlider/MainSlider.tsx
+++ src/components/MainSlider/MainSlider.tsx
@@ -1,11 +1,6 @@
 "use client";
 import Carousel from 'react-bootstrap/Carousel';
 
-import Image from 'next/image';
-import Image1 from "../../../public/images/image 1.png"
-import Image2 from "../../../public/images/image 2.png"
-import Image3 from "../../../public/images/image 3.png"
-
 import "./MainSlider.css"
 
 export default function MainSlider() {
@@ -13,9 +8,7 @@ export default function MainSlider() {
     <div className="px-28 pt-5 main-slider">
       <Carousel>
         <Carousel.Item>
-          <div className='slider__main-1 w-full h-[500px]'>
-            <Image src={Image1} alt='Picture' className='h-full w-full object-cover' />
-          </div>
+          <div className='slider__main-1 w-full h-[500px]'></div>
           <Carousel.Caption>
             <div>
               <p>Social Development</p>
@@ -25,9 +18,7 @@ export default function MainSlider() {
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <div className='slider__main-2 w-full h-[500px]'>
-            <Image src={Image2} alt='Picture' className='h-full w-full object-cover' />
-          </div>
+          <div className='slider__main-2 w-full h-[500px]'></div>
           <Carousel.Caption>
             <div>
               <p>Social Development</p>
@@ -37,9 +28,7 @@ export default function MainSlider() {
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <div className='slider__main-3 w-full h-[500px]'>
-            <Image src={Image3} alt='Picture' className='h-full w-full object-cover' />
-          </div>
+          <div className='slider__main-3 w-full h-[500px]'></div>
           <Carousel.Caption>
             <div>
               <p>Social Development</p>
